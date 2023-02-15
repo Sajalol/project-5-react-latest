e9a330d26dc5e25c23d1790de456b91d2ec0e1b1
Stian Lystad <Stianlystad_1@hotmail.com>
2023-02-15 14:01:48 +0000

    added console.logs and fixed date error. commented out attachements and made create task work

--- src/pages/posts/CreateTask.js
+++ src/pages/posts/CreateTask.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import styles from '../../styles/CreateTask.module.css';
 import btnStyles from "../../styles/Button.module.css";
 
-import { Form, Button, Select, Input, DatePicker, Checkbox } from 'antd';
+import { Select, Input, DatePicker, Checkbox } from 'antd';
 import moment from 'moment';
 import axios from 'axios';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
@@ -17,11 +17,23 @@ const CreateTask = () => {
     due_date: moment(),
     created_by: currentUser?.id,
     assigned_to: '',
-    attachements: '',
+    // attachements: '',
     category: '',
     priority: 5,
     completed: false,
   });
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rest-api-project5.herokuapp.com/todo/users/')
+      .then(res => {
+        setUsers(res.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   const onFormChange = (event) => {
     const { name, value } = event.target;
@@ -31,7 +43,7 @@ const CreateTask = () => {
   const onDueDateChange = (date, dateString) => {
     setFormData({
       ...formData,
-      due_date: moment(date).format("dd,MM,YYYY"),
+      due_date: moment(date).format("YYYY-MM-DD"),
     });
   };
 
@@ -43,82 +55,92 @@ const CreateTask = () => {
     setFormData({ ...formData, priority: value });
   };
 
+  const onAssignedToChange = (value) => {
+    setFormData({ ...formData, assigned_to: value });
+  };
+
   const onCompletedChange = (event) => {
     setFormData({ ...formData, completed: event.target.checked });
   };
-// TODO: Add logic to send formData to Django API endpoint
+
   const onFormSubmit = (event) => {
-    console.log('form submitted')
     event.preventDefault();
+    console.log("formData:", formData);
+    console.log("users:", users);
     axios.post('https://rest-api-project5.herokuapp.com/todo/task-create/', formData)
-        .then(res => {
-            console.log(res.data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+      .then(res => {
+        console.log('Task created successfully:', res.data);
+      })
+      .catch(error => {
+        console.error('Error creating task:', error);
+      });
   };
 
   return (
     <div className={styles.formContainer}>
-    <form onSubmit={onFormSubmit}>
-      <Form.Item label="Title">
+      <form onSubmit={onFormSubmit}>
+        <label>Title:</label>
         <Input
           name="title"
           value={formData.title}
-          onChange={(event) => setFormData({ ...formData, [event.target.name]: event.target.value })}
+          onChange={onFormChange}
           required
         />
-      </Form.Item>
-      <Form.Item label="Content">
+
+        <label>Content:</label>
         <Input.TextArea
           name="content"
           value={formData.content}
-          onChange={(event) => setFormData({ ...formData, [event.target.name]: event.target.value })}
+          onChange={onFormChange}
         />
-      </Form.Item>
-      <Form.Item label="Due Date">
+
+        <label>Due Date:</label>
         <DatePicker
           name="due_date"
           value={moment(formData.due_date)}
           onChange={onDueDateChange}
         />
-      </Form.Item>
-      <Form.Item label="Created By">
+
+        <label>Created By:</label>
         <Input
           name="created_by"
           value={formData.created_by}
           disabled
         />
-      </Form.Item>
-      <Form.Item label="Assigned To">
-        <Input
+
+        <label>Assigned To:</label>
+        <Select
           name="assigned_to"
           value={formData.assigned_to}
-          onChange={(event) => setFormData({ ...formData, [event.target.name]: event.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Attachements">
+          onChange={onAssignedToChange}
+        >
+          {users.map(user => (
+            <Option key={user.id} value={user.id}>{user.username}</Option>
+          ))}
+        </Select>
+
+
+        {/* <label>Attachements:</label>
         <Input
           name="attachements"
           value={formData.attachements}
-          onChange={(event) => setFormData({ ...formData, [event.target.name]: event.target.value })}
-        />
-      </Form.Item>
-      <Form.Item label="Category">
+          onChange={onFormChange}
+          />  */}
+
+        <label>Category:</label>
         <Select
           name="category"
           value={formData.category}
           onChange={onCategoryChange}
         >
-            <Option value="0">Backend</Option>
-            <Option value="1">Frontend</Option>
-            <Option value="2">Database</Option>
-            <Option value="3">Python</Option>
-            <Option value="4">Javascript</Option>
-            </Select>
-            </Form.Item>
-            <Form.Item label="Priority">
+          <Option value="0">Backend</Option>
+          <Option value="1">Frontend</Option>
+          <Option value="2">Database</Option>
+          <Option value="3">Python</Option>
+          <Option value="4">Javascript</Option>
+        </Select>
+
+        <label>Priority:</label>
         <Select
           name="priority"
           value={formData.priority}
@@ -129,24 +151,19 @@ const CreateTask = () => {
           <Option value={3}>3</Option>
           <Option value={4}>4</Option>
           <Option value={5}>5</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item label="Completed">
-        <Checkbox
-          name="completed"
-          checked={formData.completed}
-          onChange={onCompletedChange}
-        />
-      </Form.Item>
-      <Form.Item>
-            {/* <Button type="submit" className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}>
-            Submit
-            </Button> */}
-            <input type="submit" value="Submit" />
-      </Form.Item>
-    </form>
-   </div>
-  );
-}
+          </Select>
+          <label>Completed:</label>
+              <Checkbox
+                name="completed"
+                checked={formData.completed}
+                onChange={onCompletedChange}
+              />
 
-export default CreateTask;
+              <input type="submit" value="Submit" className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} />
+
+            </form>
+          </div>
+          );
+        };
+        
+        export default CreateTask;
--- src/pages/posts/ToDoList.js
+++ src/pages/posts/ToDoList.js
@@ -4,48 +4,49 @@ import styles from '../../styles/ToDoList.module.css';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 const TodoList = () => {
-    const [tasks, setTasks] = useState([]);
-    const [error, setError] = useState(null);
-    const currentUser = useCurrentUser();
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+  const currentUser = useCurrentUser();
 
-    useEffect(() => {
-        if (currentUser) {
-            axios.get('https://rest-api-project5.herokuapp.com/todo/task-list/')
-                .then(res => {
-                    console.log(res.data);
-                    setTasks(res.data.results);
-                })
-                .catch(error => {
-                    console.error(error);
-                    setError('Could not fetch tasks');
-                });
-        }
-    }, [currentUser]);
+  useEffect(() => {
+    if (currentUser) {
+      axios.get('https://rest-api-project5.herokuapp.com/todo/task-list/')
+        .then(res => {
+          console.log(res.data);
+          setTasks(res.data.results);
+        })
+        .catch(error => {
+          console.error(error);
+          setError('Could not fetch tasks');
+        });
+    }
+  }, [currentUser]);
 
+  if (error) return <p>{error}</p>;
 
+  const completedTasks = tasks.filter(task => !task.completed);
 
-    if (error) return <p>{error}</p>;
-
-    return tasks.length > 0
-        ? (
-            <ul className={styles.taskList}>
-                {
-                    tasks.map(task => {
-                        return (
-                            <li key={task.id} className={styles.task}>
-                                <h3 className={styles.taskTitle}>{task.title}</h3>
-                                <p className={styles.taskDueDate}>Due Date: {task.due_date}</p>
-                                <p className={styles.taskAssignedTo}>Assigned to: {task.assigned_to}</p>
-                                <p className={styles.taskCategory}>Category: {task.category}</p>
-                                <p className={styles.taskPriority}>Priority: {task.priority}</p>
-                                <p className={styles.taskCompleted}>Completed: {task.completed ? 'Yes' : 'No'}</p>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        )
-        : <p>No tasks to show</p>;
+  return (
+    <div>
+      <p>Total tasks: {tasks.length}</p>
+      <ul className={styles.taskList}>
+        {completedTasks.length > 0 ? (
+          completedTasks.map(task => (
+            <li key={task.id} className={styles.task}>
+              <h3 className={styles.taskTitle}>{task.title}</h3>
+              <p className={styles.taskDueDate}>Due Date: {task.due_date}</p>
+              <p className={styles.taskAssignedTo}>Assigned to: {task.assigned_to}</p>
+              <p className={styles.taskCategory}>Category: {task.category}</p>
+              <p className={styles.taskPriority}>Priority: {task.priority}</p>
+              <p className={styles.taskCompleted}>Completed: {task.completed ? 'Yes' : 'No'}</p>
+            </li>
+          ))
+        ) : (
+          <p>No tasks to show</p>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
