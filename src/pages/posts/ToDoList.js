cdeb989c5a4fb26d98a3c36a2fcc01a1f74b02a5
Stian Lystad <Stianlystad_1@hotmail.com>
2023-02-19 14:37:17 +0000

    added edit button to progress bar

--- src/pages/posts/ToDoList.js
+++ src/pages/posts/ToDoList.js
@@ -11,6 +11,20 @@ const TodoList = () => {
   const [priorityFilter, setPriorityFilter] = useState(0); // default to no filter
   const [sortOrder, setSortOrder] = useState('asc'); // default to ascending order
   const [sortField, setSortField] = useState('priority'); // default to sorting by priority
+
+  const updateTask = async (taskId, completedPercentage) => {
+    try {
+      const response = await axios.put(
+        `https://rest-api-project5.herokuapp.com/todo/task-update/${taskId}/`,
+        { completed_percentage: completedPercentage }
+      );
+      // Update the tasks state with the updated task
+      setTasks(tasks => tasks.map(task => task.id === taskId ? response.data : task));
+    } catch (error) {
+      console.error(error);
+      setError('Could not update task');
+    }
+  }
   const currentUser = useCurrentUser();
 
   useEffect(() => {
@@ -81,63 +95,65 @@ const TodoList = () => {
           type="text"
           placeholder="Search tasks..."
           value={searchTerm}
-          onChange={event => setSearchTerm(event.target.value)}
-        />
-      </div>
-      <div className={styles.filtersContainer}>
-      <div className={styles.priorityFilter}>
-          <label>Priority filter:</label>
-          <select value={priorityFilter} onChange={event => setPriorityFilter(parseInt(event.target.value))}>
-            <option value={0}>No filter</option>
-            {[1, 2, 3, 4, 5].map(value => (
-              <option key={value} value={value}>{value}</option>
-            ))}
-          </select>
+          onChange={event =>setSearchTerm(event.target.value)}
+          />
         </div>
-        <div className={styles.sortOrder}>
-          <label>Sort by:</label>
-          <select value={sortField} onChange={event => setSortField(event.target.value)}>
-            <option value="priority">Priority</option>
-            <option value="due_date">Due date</option>
-          </select>
+        <div className={styles.filtersContainer}>
+        <div className={styles.priorityFilter}>
+            <label>Priority filter:</label>
+            <select value={priorityFilter} onChange={event => setPriorityFilter(parseInt(event.target.value))}>
+              <option value={0}>No filter</option>
+              {[1, 2, 3, 4, 5].map(value => (
+                <option key={value} value={value}>{value}</option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.sortOrder}>
+            <label>Sort by:</label>
+            <select value={sortField} onChange={event => setSortField(event.target.value)}>
+              <option value="priority">Priority</option>
+              <option value="due_date">Due date</option>
+            </select>
+          </div>
+          <div className={styles.sortOrder}>
+            <label>Sort date:</label>
+            <select value={sortOrder} onChange={event => setSortOrder(event.target.value)}>
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
         </div>
-        <div className={styles.sortOrder}>
-          <label>Sort date:</label>
-          <select value={sortOrder} onChange={event => setSortOrder(event.target.value)}>
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-          </select>
-        </div>
-      </div>
-      <p>Total tasks: {filteredTasksByTitle.length}</p>
-      <ul className={styles.taskList}>
-      {filteredTasksByTitle.length > 0 ? (
-          filteredTasksByTitle.map(task => (
-            <li key={task.id} className={styles.task}>
-              <h3 className={styles.taskTitle}>{task.title}</h3>
-              <p className={styles.taskDueDate}>Due Date: {task.due_date}</p>
-              <p className={styles.taskAssignedTo}>Assigned to: {users[task.assigned_to]?.username}</p>
-              <p className={styles.taskCategory}>Category: {task.category}</p>
-              <p className={styles.taskPriority}>Priority: {task.priority}</p>
-              <div className={styles.taskProgress}>
-                <div className={styles.taskProgressLabel}>Percent completed:</div>
-                <div className={styles.taskProgressBarContainer}>
-                  <div
-                    className={styles.taskProgressBar}
-                    style={{ width: `${Math.max(task.completed_percentage, 5)}%` }}
-                  />
-                  <div className={styles.taskProgressValue}>{task.completed_percentage}%</div>
+        <p>Total tasks: {filteredTasksByTitle.length}</p>
+        <ul className={styles.taskList}>
+          {filteredTasksByTitle.length > 0 ? (
+            filteredTasksByTitle.map(task => (
+              <li key={task.id} className={styles.task}>
+                <h3 className={styles.taskTitle}>{task.title}</h3>
+                <p className={styles.taskDueDate}>Due Date: {task.due_date}</p>
+                <p className={styles.taskAssignedTo}>Assigned to: {users[task.assigned_to]?.username}</p>
+                <p className={styles.taskCategory}>Category: {task.category}</p>
+                <p className={styles.taskPriority}>Priority: {task.priority}</p>
+                <div className={styles.taskProgress}>
+                  <div className={styles.taskProgressLabel}>Percent completed:</div>
+                  <div className={styles.taskProgressBarContainer}>
+                    <div
+                      className={styles.taskProgressBar}
+                      style={{ width: `${Math.max(task.completed_percentage, 5)}%` }}
+                    />
+                    <button className={styles.taskProgressButton} onClick={() => updateTask(task.id, task.completed_percentage + 10)}>
+                      +10%
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <p className={styles.taskCompleted}>Completed: {task.completed ? 'Yes' : 'No'}</p>
-            </li>
-          ))
-        ) : (
-          <p>No tasks to show</p>
-        )}
-      </ul>
-    </div>
-  );
-};
+                <p className={styles.taskCompleted}>Completed: {task.completed ? 'Yes' : 'No'}</p>
+              </li>
+            ))
+          ) : (
+            <p>No tasks to show</p>
+          )}
+        </ul>
+      </div>
+    );
+  };
 
-export default TodoList;
+  export default TodoList;
