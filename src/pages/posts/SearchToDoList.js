5e21627f0d2bc0647d2d0dfa985e83ffce3c1733
Stian Lystad <Stianlystad_1@hotmail.com>
2023-02-23 00:10:44 +0000

    added new style and functionality to searchtodolist

--- src/pages/posts/SearchToDoList.js
+++ src/pages/posts/SearchToDoList.js
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from '../../styles/ToDoList.module.css';
-import { Select } from 'antd';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
-const { Option } = Select;
-
-const UserTasksList = ({ userId }) => {
+const SearchToDoList = () => {
   const [tasks, setTasks] = useState([]);
   const [error, setError] = useState(null);
-  const [users, setUsers] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
+  const [users, setUsers] = useState({});
   const [priorityFilter, setPriorityFilter] = useState(0); // default to no filter
+  const [assignedToFilter, setAssignedToFilter] = useState(0); // default to no filter
   const [sortOrder, setSortOrder] = useState('asc'); // default to ascending order
   const [sortField, setSortField] = useState('priority'); // default to sorting by priority
-  const [filterUser, setFilterUser] = useState('');
-
   const CATEGORIES_DICT = {
     0: 'Backend',
     1: 'Frontend',
@@ -23,123 +20,180 @@ const UserTasksList = ({ userId }) => {
     4: 'Javascript',
   };
 
-  // Fetch the list of tasks from the server
+  const updateTask = async (taskId, completedPercentage) => {
+    try {
+      completedPercentage = Math.max(0, Math.min(100, completedPercentage)); // Constrain completedPercentage to between 0 and 100
+      const response = await axios.put(
+        `https://rest-api-project5.herokuapp.com/todo/task-update/${taskId}/`,
+        { completed_percentage: completedPercentage }
+      );
+      // Update the tasks state with the updated task
+      setTasks(tasks => tasks.map(task => task.id === taskId ? response.data : task));
+    } catch (error) {
+      console.error(error);
+      setError('Could not update task');
+    }
+  }
+  const currentUser = useCurrentUser();
+
   useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        let url = `https://rest-api-project5.herokuapp.com/todo/task-list/${userId}/`;
-        if (priorityFilter > 0) {
-          url += `?priority=${priorityFilter}`;
+    const getUsers = async () => {
+      const response = await axios.get('https://rest-api-project5.herokuapp.com/todo/users/');
+      const usersData = response.data.reduce((acc, user) => {
+        acc[user.id] = user;
+        return acc;
+      }, {});
+      setUsers(usersData);
+    };
+    getUsers();
+
+    if (currentUser) {
+      const getTasks = async () => {
+        try {
+          const results = [];
+          let nextPage = 'https://rest-api-project5.herokuapp.com/todo/task-list/';
+
+          while (nextPage) {
+            const res = await axios.get(nextPage);
+            results.push(...res.data.results);
+            nextPage = res.data.next;
+          }
+
+          setTasks(results);
+        } catch (error) {
+          console.error(error);
+          setError('Could not fetch tasks');
         }
-        const response = await axios.get(url);
-        setTasks(response.data);
-      } catch (error) {
-        console.error(error);
-        setError('Could not fetch tasks');
-      }
-    };
+      };
 
-    fetchTasks();
-  }, [userId, priorityFilter]);
-
-  // Fetch the list of users from the server
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get('https://rest-api-project5.herokuapp.com/todo/users/');
-        setUsers(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  // Handle the search term input
-  const handleSearchTermChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  // Handle the priority filter select
-  const handlePriorityFilterChange = (value) => {
-    setPriorityFilter(value);
-  };
-
-  // Handle the user filter select
-  const handleFilterUserChange = (value) => {
-    setFilterUser(value);
-  };
-
-  // Get the list of task items to render, after applying filters
-  const getFilteredTasks = () => {
-    let filteredTasks = tasks.filter(task => task.title.toLowerCase().includes(searchTerm.toLowerCase()));
-    if (filterUser !== '') {
-      filteredTasks = filteredTasks.filter(task => task.assigned_to === parseInt(filterUser));
+      getTasks();
     }
-    return filteredTasks;
-  };
+  }, [currentUser]);
 
-  // Render the list of tasks
-  const renderTaskList = () => {
-    const filteredTasks = getFilteredTasks();
-    if (filteredTasks.length === 0) {
-      return <p>No tasks found.</p>;
-    }
-    return (
-      <div>
-        <h2>Tasks:</h2>
-        <div className={styles.filters}>
-          <label>Search:</label>
-          <input type="text" value={searchTerm} onChange={handleSearchTermChange} />
-          <label>Priority:</label>
-          <Select value={priorityFilter} onChange={handlePriorityFilterChange}>
-            <Option value={0}>All</Option>
-            <Option value={1}>1</Option>
-            <Option value={2}>2</Option>
-            <Option value={3}>3</Option>
-            <Option value={4}>4</Option>
-            </Select>
-          <label>User:</label>
-          <Select value={filterUser} onChange={handleFilterUserChange}>
-            <Option value="">All</Option>
-            {Object.keys(users).map(key => (
-              <Option key={key} value={key}>{users[key]}</Option>
-            ))}
-          </Select>
-        </div>
-        <table className={styles.taskList}>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Category</th>
-              <th>Priority</th>
-              <th>Assigned to</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredTasks.map(task => (
-              <tr key={task.id}>
-                <td>{task.title}</td>
-                <td>{task.description}</td>
-                <td>{CATEGORIES_DICT[task.category]}</td>
-                <td>{task.priority}</td>
-                <td>{users[task.assigned_to]}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  };
+  if (error) return <p>{error}</p>;
 
-  return (
-    <div className={styles.userTasksList}>
-      {error && <p>{error}</p>}
-      {renderTaskList()}
-    </div>
-  );
+  const completedTasks = tasks.filter(task => !task.completed);
+
+  // apply priority filter if set
+  const filteredTasks = priorityFilter > 0
+    ? completedTasks.filter(task => task.priority === priorityFilter)
+    : completedTasks;
+
+  // apply assigned to filter if set
+  const filteredTasksByUser = assignedToFilter > 0
+    ? filteredTasks.filter(task => task.assigned_to === assignedToFilter)
+    : filteredTasks;
+
+  // apply sort order and field if set
+const sortedFilteredTasks = filteredTasksByUser.sort((a, b) => {
+  const fieldToSortBy = sortField === 'category'
+    ? task => CATEGORIES_DICT[task.category]
+    : task => task[sortField];
+
+  const valA = fieldToSortBy(a);
+  const valB = fieldToSortBy(b);
+
+  if (valA > valB) {
+    return sortOrder === 'asc' ? 1 : -1;
+  } else if (valB > valA) {
+    return sortOrder === 'asc' ? -1 : 1;
+  } else {
+    return 0;
+  }
+});
+
+// determine which field to sort by
+let fieldToSortBy;
+if (sortField === 'category') {
+fieldToSortBy = task => task.category;
+} else if (sortField === 'assigned_to') {
+fieldToSortBy = task => users[task.assigned_to]?.username;
+} else {
+fieldToSortBy = task => task[sortField];
+}
+
+// sort tasks based on sort order and field
+const sortedTasks = sortOrder === 'asc'
+  ? sortedFilteredTasks.sort((a, b) => fieldToSortBy(a) > fieldToSortBy(b) ? 1 : -1)
+  : sortedFilteredTasks.sort((a, b) => fieldToSortBy(a) < fieldToSortBy(b) ? 1 : -1);
+
+return (
+<div className={styles.todoList}>
+<h1>Todo List</h1>
+<div className={styles.filters}>
+<div className={styles.filter}>
+<label htmlFor="search">Search:</label>
+<input type="text" id="search" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
+</div>
+<div className={styles.filter}>
+<label htmlFor="priority">Priority:</label>
+<select id="priority" value={priorityFilter} onChange={event => setPriorityFilter(Number(event.target.value))}>
+<option value="0">No filter</option>
+{[1, 2, 3].map(priority => (
+<option key={priority} value={priority}>{priority}</option>
+))}
+</select>
+</div>
+<div className={styles.filter}>
+<label htmlFor="assignedTo">Assigned to:</label>
+<select id="assignedTo" value={assignedToFilter} onChange={event => setAssignedToFilter(Number(event.target.value))}>
+<option value="0">No filter</option>
+{Object.values(users).map(user => (
+<option key={user.id} value={user.id}>{user.username}</option>
+))}
+</select>
+</div>
+<div className={styles.filter}>
+<label htmlFor="sortField">Sort by:</label>
+<select id="sortField" value={sortField} onChange={event => setSortField(event.target.value)}>
+<option value="priority">Priority</option>
+<option value="due_date">Due Date</option>
+<option value="category">Category</option>
+<option value="assigned_to">Assigned To</option>
+</select>
+</div>
+<div className={styles.filter}>
+<label htmlFor="sortOrder">Sort order:</label>
+<select id="sortOrder" value={sortOrder} onChange={event => setSortOrder(event.target.value)}>
+<option value="asc">Ascending</option>
+<option value="desc">Descending</option>
+</select>
+</div>
+</div>
+<table>
+<thead>
+<tr>
+<th>ID</th>
+<th>Title</th>
+<th>Description</th>
+<th>Priority</th>
+<th>Due Date</th>
+<th>Category</th>
+<th>Assigned To</th>
+<th>Completed Percentage</th>
+</tr>
+</thead>
+<tbody>
+{sortedTasks
+.filter(task => task.title.toLowerCase().includes(searchTerm.toLowerCase()))
+.map(task => (
+<tr key={task.id}>
+<td>{task.id}</td>
+
+<td>{task.title}</td>
+<td>{task.description}</td>
+<td>{task.priority}</td>
+<td>{task.due_date}</td>
+<td>{CATEGORIES_DICT[task.category]}</td>
+<td>{users[task.assigned_to]?.username || 'Unassigned'}</td>
+<td>
+  <input type="number" min="0" max="100" value={task.completed_percentage} onChange={event => updateTask(task.id, event.target.value)} />
+</td>
+</tr>
+))}
+</tbody>
+</table>
+</div>
+);
 };
-
-export default UserTasksList;
+export default SearchToDoList;
